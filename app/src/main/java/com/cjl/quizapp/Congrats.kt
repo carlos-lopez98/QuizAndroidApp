f16a3b9d29740f4130e28fe1dd4de15177ff9d26
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-02-13 17:36:24 -0800

    Added Result Screen

---
+++ app/src/main/java/com/cjl/quizapp/Congrats.kt
@@ -0,0 +1,19 @@
+package com.cjl.quizapp
+
+import android.graphics.Color
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.*
+import androidx.core.content.ContextCompat
+
+class Congrats : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
